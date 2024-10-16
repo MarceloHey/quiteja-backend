c0ae9d6c1ca37f8594e6194139943e703b01c34a
hey-quiteja <marcelo.hey@quiteja.com.br>
2024-10-16 11:15:03 -0300

    feature: debts api

--- debts-api/routes/routes.py
+++ debts-api/routes/routes.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from typing import Optional
 from flask import Blueprint, request, jsonify
 from models.models import Debt
 from app import db
@@ -20,3 +22,80 @@ def list_debts():
             for d in debts
         ]
     )
+
+@debt.route("/", methods=["POST"])
+def create_debt():
+    data = request.get_json()
+
+    existing_user_debt = Debt.query.filter_by(user_id=data["user_id"]).first()
+    if existing_user_debt:
+        return jsonify({"error": "Usuario ja tem divida"}), 400
+
+    debt = Debt(
+        product=data["product"],
+        value=data["value"],
+        user_id=data["user_id"],
+        due_date=datetime.strptime(data["due_date"], "%d-%m-%Y").date(),
+    )
+
+    db.session.add(debt)
+    db.session.commit()
+
+    return (
+        jsonify(
+            {
+                "message": "Dívida criada com sucesso",
+                "debt": {
+                    "id": debt.id,
+                    "product": debt.product,
+                    "value": debt.value,
+                    "due_date": debt.due_date,
+                },
+            }
+        ),
+        201,
+    )
+
+
+@debt.route("/<int:debt_id>", methods=["PATCH"])
+def update_debt(debt_id):
+    data = request.get_json()
+
+    debt: Optional[Debt] = Debt.query.filter_by(id=debt_id).first()
+    if not debt:
+        return jsonify({"error": "Dívida não encontrada"}), 400
+    
+    debt.due_date = datetime.strptime(data["due_date"], "%d-%m-%Y").date()
+    debt.product = data["product"]
+    debt.value = data["value"]
+
+    db.session.add(debt)
+    db.session.commit()
+
+    return (
+        jsonify(
+            {
+                "message": "Dívida editada com sucesso",
+                "debt": {
+                    "id": debt.id,
+                    "product": debt.product,
+                    "value": debt.value,
+                    "due_date": debt.due_date,
+                },
+            }
+        ),
+        201,
+    )
+
+@debt.route("/<int:debt_id>", methods=["DELETE"])
+def delete_debt(debt_id):
+    debt: Optional[Debt] = Debt.query.filter_by(id=debt_id).first()
+    if not debt:
+        return jsonify({"error": "Dívida não encontrada"}), 400
+
+    debtId = debt.id
+    Debt.query.filter_by(id=debtId).delete()
+
+    db.session.commit()
+
+    return jsonify({"message": f"Dívida {debtId} removida com sucesso"}), 200
